90c779ee45d12aa1802b86484e0fe052792b94ca
ahmeetarif <ahmetarif1254@gmail.com>
2022-08-14 16:29:01 +0300

    ContainsDuplicate - Problem Fixed

--- array_hashing/LeetCodeProblems.ContainsDuplicate/Program.cs
+++ array_hashing/LeetCodeProblems.ContainsDuplicate/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool ContainsDuplicate(int[] nums)
+{
+    var hashedArray = nums.ToHashSet<int>();
+    if (nums.Length != hashedArray.Count)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool ContainsDuplicate2(int[] nums)
+{
+    List<int> prevMap = new List<int>();
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (prevMap.Contains(nums[i]))
+        {
+            return true;
+        }
+
+        prevMap.Add(nums[i]);
+    }
+
+    return false;
+}
